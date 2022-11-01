b919692ae311a06d5728fdca4e9cd8f96604cdea
GMoreira05 <henrique_gabriel11@outlook.com>
2022-11-01 12:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ AppConsultaCEP/AppConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppConsultaCEP.View;
+
+namespace AppConsultaCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async btn_CEP_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConsultaCEP());
+        }
+    }
+}
